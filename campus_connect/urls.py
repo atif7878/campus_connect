a1767134348cdf25adf20af0ed0a56b4c9a4233f
atif7878 <itsatifkhan871@gmail.com>
2025-10-22 23:24:19 +0500

    dfsdfsdf

--- campus_connect/urls.py
+++ campus_connect/urls.py
@@ -4,9 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from core.views import HomeView, SignupView, CustomLoginView, CustomLogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', HomeView.as_view(), name='home'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
 
 # Static files - only development mein
