db1f472a4c07887ec085586c002b72962a4edd00
atif7878 <itsatifkhan871@gmail.com>
2025-10-22 18:54:24 +0500

    dfsdfsdf

--- campus_connect/settings.py
+++ campus_connect/settings.py
@@ -95,7 +95,10 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'PORT': '25870',
+        'OPTIONS': {
+            'sslmode': 'require',  # Aiven requires SSL
+        },
     }
 }
 
